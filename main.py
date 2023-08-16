08a30061c30a51f26890508ac4dbfd1b8a432098
Kevin <myservices@kabes.org>
2023-08-16 17:13:34 -0400

    * Add ability to generate voice audio using bark model * installed pytorch, hugging face transformers, scipy, and nltk

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 from dotenv import load_dotenv
 from webscraper import generate_input
+from voice_generator import voicegen
 
 
 def main():
     # Load the environment variables
     load_dotenv()
-    print(os.environ.get("URL"))
     # Scrap the data from the website and save to a file
     generate_input.generate_input_files(os.environ.get("URL"))
+    # Generate voice if the input file exists from file text
+    if os.path.exists("temporary/input.txt"):
+        with open("temporary/input.txt", "r") as f:
+            text = f.read()
+        voicegen.generate_voice_from_text(text, os.environ.get("VOICE_PRESET"))
+    else:
+        raise Exception("Error: Input file does not exist!!")
+    
 
 
 if __name__ == "__main__":
